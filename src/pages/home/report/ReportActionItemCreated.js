d12bf563ff0bcf90bbe2b06cf4ca7e1e10027905
sig5 <singhalsakar@gmail.com>
2022-01-27 12:37:19 +0530

    fixed imports

--- src/pages/home/report/ReportActionItemCreated.js
+++ src/pages/home/report/ReportActionItemCreated.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native-web';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import EmptyStateAvatars from '../../../components/EmptyStateAvatars';
 import ReportWelcomeText from '../../../components/ReportWelcomeText';
