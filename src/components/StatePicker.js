abc1976bed8353ac3ffdd31370140ebfa806282f
Sakar Singhal <singhalsakar@gmail.com>
2022-02-04 13:17:52 +0530

    Added props

--- src/components/StatePicker.js
+++ src/components/StatePicker.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {CONST} from 'expensify-common/lib/CONST';
 import Picker from './Picker';
+import * as FormUtils from '../libs/FormUtils';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
 const STATES = _.map(CONST.STATES, ({stateISO}) => ({
@@ -20,12 +21,28 @@ const propTypes = {
     /** The value that needs to be selected */
     value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
+    /** Indicates that the input is being used with the Form component */
+    isFormInput: PropTypes.bool,
+
+    /**
+     * The ID used to uniquely identify the input
+     *
+     * @param {Object} props - props passed to the input
+     * @returns {Object} - returns an Error object if isFormInput is supplied but inputID is falsey or not a string
+     */
+    inputID: props => FormUtils.getInputIDPropTypes(props),
+
+    /** Saves a draft of the input value when used in a form */
+    shouldSaveDraft: PropTypes.bool,
+
     ...withLocalizePropTypes,
 };
 
 const defaultProps = {
     label: '',
     value: '',
+    isFormInput: false,
+    shouldSaveDraft: false,
 };
 
 const StatePicker = props => (
